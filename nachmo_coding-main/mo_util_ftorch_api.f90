c943844f6c8fbea214dc7c0a9841d03042ed1474
tpccc <budas2kh@gmail.com>
2024-06-06 20:06:22 +0200

    Correct reference to device_type and device_index

--- nachmo_coding-main/mo_util_ftorch_api.f90
+++ nachmo_coding-main/mo_util_ftorch_api.f90
@@ -5,7 +5,7 @@ module mo_util_ftorch_api
 
     private
 
-    TYPE :: torch_net
+    TYPE :: ftorch_net
         ! Universal class object for handling trained Torch Model in Torch Script form
         ! For details on FTorch see github repo FTorch
         ! Attributes:
@@ -21,8 +21,7 @@ module mo_util_ftorch_api
         CONTAINS
         procedure :: load_model => model_load
         procedure :: emulate => inference
-    END TYPE nachmo_coding
-
+    END TYPE ftorch_net
     public :: nachmo_coding
 
     CONTAINS
@@ -37,19 +36,25 @@ module mo_util_ftorch_api
         self % model = torch_module_load(path)
     end subroutine load_model
 
-    subroutine emulate(self, arr, arr_out, n_inputs)
+    subroutine emulate(self, arr, arr_out, n_inputs, device_type, device_index)
         ! Emulate Chemical reaction based on nachmo_mlp.
         ! Input:
         !       arr (array, allocatable)        : Input array object, column major
         !       arr_out (array, allocatable)    : Empty output array object, column major
         !       n_inptus (integer)              : Number of input tensors
+        !       device_type (c_int)             : Device type for which the target tensor lives on
+        !                                         ('torch_kCPU', 'torch_kCUDA')
         ! Return:
         !       arr_out (array, allocatable)    : Output of the NN emulation, column major
 
+        use, intrinsic :: iso_c_binding, only : c_int
+        ! Defining input types
         class(nachmo_coding), intent(inout) :: self
         real(wp), dimension(:), allocatable, intent(in) :: arr
         real(wp), dimension(:), allocatable, intent(inout) :: arr_out
         integer, intent(in) :: n_inputs
+        integer(c_int), intent(in)     :: device_type
+        integer(c_int), optional, intent(in) :: device_index 
 
         type(torch_tensor), dimension(:), allocatable :: model_inputs_arr
         type(torch_tensor) :: model_output
@@ -65,7 +70,7 @@ module mo_util_ftorch_api
         allocate(in_layout(in_dims))
         allocate(out_layout(out_dims))
 
-        ! Setting layout for dynamic dimensions
+        ! Setting layout for dynamic dimensions from arr and arr_out
         do i = 1, in_dims
             in_layout(i) = i
         end do
@@ -74,10 +79,19 @@ module mo_util_ftorch_api
             out_layout(i) = i
         end do
 
+        ! Process optional arguments
+        if (present(device_index)) then
+            device_index_value = device_index
+        else if (device_type == torch_kCPU) then
+            device_index_value = -1
+        else
+            device_index_value = 0
+        endif
+
         ! Transpose the inpt array from column major (Fortran style) to row major (C++ style)
-        model_inputs_arr(1) = torch_tensor_from_array(arr, in_layout, torch_kCUDA,   &
+        model_inputs_arr(1) = torch_tensor_from_array(arr, in_layout, device_type,   &
                                                         device_index=device_index)
-        model_output = torch_tensor_from_array(arr_out, out_layout, torch_kCUDA,   &
+        model_output = torch_tensor_from_array(arr_out, out_layout, device_type,   &
                                                         device_index=device_index)
 
         ! Call the foward method of the loaded model, return the row major model_output object
